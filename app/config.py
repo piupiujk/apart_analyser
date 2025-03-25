a61425f74f313e0f2d4784e921fcbfcc7c9b7710
piupiujk <docs4p@yandex.ru>
2025-03-25 20:29:10 +0500

    docker7

--- app/config.py
+++ app/config.py
@@ -11,6 +11,10 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    POSTGRES_DB: str | None = None
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
+
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env",
         env_file_encoding='utf-8',
@@ -22,5 +26,14 @@ settings = Settings()
 
 
 def get_db_url():
-    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@"
-            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+    if settings.POSTGRES_USER and settings.POSTGRES_PASSWORD:
+        # Используем параметры из docker-compose
+        return (
+            f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
+            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.POSTGRES_DB or settings.DB_NAME}"
+        )
+        # Используем стандартные параметры
+    return (
+        f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@"
+        f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+    )
