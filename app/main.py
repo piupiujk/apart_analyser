9e65710e8ef21ca400265074515bac6155784f02
root <root@cv4603159.novalocal>
2025-04-05 09:57:49 +0300

    move docs to api/docs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
-from app.routers import router as main_router
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from app.routers import router as api_router
 
-app = FastAPI(title='Анализ квартир')
+app = FastAPI(
+    title='Анализ квартир',
+    docs_url=None,
+    redoc_url=None
+)
 
+app.include_router(api_router)
 
 @app.get('/', summary='Главная страница', tags=['Главная страница'])
 def home_page():
     return {'message': 'Главная страница'}
 
-
-app.include_router(main_router)
+@app.exception_handler(404)
+async def api_not_found_handler(request: Request, exc: HTTPException):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"success": False, "msg": "Неизвестный метод"}
+        )
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"detail": "404 page"}
+    )
--- app/routers.py
+++ app/routers.py
@@ -1,9 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 from app.users.router import router as user_router
 from app.apartments.router import router as apartment_router
 
-router = APIRouter()
+router = APIRouter(tags=["API"], prefix="/api")
 
 router.include_router(apartment_router)
 router.include_router(user_router)
+
+
+@router.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html(request: Request):
+    return get_swagger_ui_html(
+        openapi_url=f"{request.scope.get('root_path', '')}/openapi.json",
+        title="API Documentation"
+    )
+
+@router.get("/openapi.json", include_in_schema=False)
+async def get_openapi(request: Request):
+    return request.app.openapi()
