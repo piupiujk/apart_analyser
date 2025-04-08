7d9ee057103f4189b1843b5bda769435d170ff26
lenadekart <decartfamily@vk.com>
2025-04-08 19:14:15 +0500

    The router is separated separately for the corresponding task

--- app/users/router.py
+++ app/users/router.py
@@ -5,11 +5,20 @@ from app.users.repository import UserRepository
 from app.users.schemas import SUserRegister
 
 router = APIRouter(
-    prefix="/auth",
-    tags=["Auth & Пользователи"]
+    tags=["Пользователи"]
 )
+auth_router = APIRouter(
+    prefix="/auth",
+    tags=["Auth"]
+)
+router.include_router(auth_router)
 
-@router.post('/register')
+
+@router.get('/is_user_exists')
+async def is_user_exists(tg_id: int):
+    existing_user = await UserRepository.find_by_tg_id(tg_id=tg_id)
+    return {"result": existing_user is not None}
+@auth_router.post('/register')
 async def register_user(user_data: SUserRegister):
     existing_user = await UserRepository.find_one_or_none(email=user_data.email)
     if existing_user:
@@ -19,7 +28,7 @@ async def register_user(user_data: SUserRegister):
     hashed_password = get_password_hash(user_data.password)
     await UserRepository.add_user(email=user_data.email, password=hashed_password)
 
-@router.post('/login')
+@auth_router.post('/login')
 async def login_user(response: Response, user_data: SUserRegister):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
@@ -27,19 +36,3 @@ async def login_user(response: Response, user_data: SUserRegister):
     access_token = create_access_token({'sub': user.id})
     response.set_cookie('apartment_access_token', access_token, httponly=True)
     return access_token
-
-
-@router.get('/is_user_exists')
-async def is_user_exists(tg_id: int):
-    existing_user = await UserRepository.find_by_tg_id(tg_id=tg_id)
-    return {"result": existing_user is not None}
-
-# @router.post('/add_tg_id')
-# async def add_tg_id(email: str, tg_id: str):
-#     existing_user = await UserRepository.find_one_or_none(email=email)
-#     if not existing_user:
-#         raise HTTPException()
-#     if existing_user.tg_id:
-#         raise HTTPException()
-#     existing_user.tg_id = tg_id
-#     return 'Добавлено'
